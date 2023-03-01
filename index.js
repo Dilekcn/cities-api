1d21886a6125a41b931c0dd1f65031062a884752
Dilekcn <canturkdk@gmail.com>
2023-03-01 12:05:07 +0000

    gitignore added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("AWS Cities!!!"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
