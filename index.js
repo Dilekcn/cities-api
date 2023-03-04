fadecfde7bd75b4b159f764d6b85cf8161a0cd99
Dilekcn <canturkdk@gmail.com>
2023-03-04 00:49:53 +0000

    cors() added

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const router = require("express").Router();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("AWS Cities!!!"));
+const fetch = require("node-fetch");
+
+async function getCityAndDescription() {
+  try {
+    const cityRes = await fetch("http://localhost:5001/city");
+    const cityData = await cityRes.json();
+    const descriptionRes = await fetch("http://localhost:5002/description");
+    const descriptionData = await descriptionRes.json();
+    return {
+      city: cityData,
+      description: descriptionData,
+    };
+  } catch (error) {
+    return "ERROR";
+  }
+}
+
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  const { city, description } = await getCityAndDescription();
+
+  if (city === "error" || description === "error") {
+    return res.status(500).json({ message: "error" });
+  }
+
+  res.status(200).json(`${city.cityname} is ${description.description}`);
+});
+
+//app.get("/", (req, res) => res.send("AWS Cities!!!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
